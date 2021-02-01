138e1a1b2eae08aa02d985fd879aec5a6559a63b
ssuyash909 <ssuyash909@gmail.com>
2021-02-01 13:19:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 const App =() => {
+  const[showAddTask,setShowAddTask]=useState(false);
   const[tasks,setTasks] = useState([
     {
         id:1,
@@ -30,12 +31,10 @@ const App =() => {
 ])
 //Add Task
 const addTask = (task) =>{
-  console.log(task);
   const id = Math.floor(Math.random() * 10000)+1;
   const newTask ={id , ...task};
   let newTasks = [...tasks,newTask] 
   setTasks(newTasks)
-  console.log(newTasks)
 }
 
 //Delete Task
@@ -55,8 +54,8 @@ const toggleReminder= (id) => {
 
 return (
     <div className="container">
-      <Header/>
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask) } showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length>0 
       ?(<Tasks tasks={tasks} onDelete={deleteTask} 
       onToggle={toggleReminder} />):('No Tasks to show')}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,12 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('Onclick')
-    }
+const Header = ({title,onAdd,showAdd}) => {
+
     return (
         <header>
             <h1>{title}</h1>
-            <Button color ='green' text='Add'onClick={onClick} />
+            <Button color ={showAdd ? 'Red' :'Green'} text={showAdd ? 'Close' :'Add'} onClick={onAdd} />
         </header>
             
 )
